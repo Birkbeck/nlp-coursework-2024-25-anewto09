9f3121e4212e3e6106717bde8e4d1a637a13d9e0
anewto09 <anewto09@student.bbk.ac.uk>
2025-06-20 20:27:45 +0100

    Filtering to 4 most common parties

--- PartTwo.py
+++ PartTwo.py
@@ -4,17 +4,21 @@ import pandas as pd
 DATASET_PATH = pathlib.Path(__file__).parent / "p2-texts" / "hansard40000.csv"
 
 def clean_hansard(df: pd.DataFrame, n_parties: int = 4) -> pd.DataFrame:
+    # 2(a) i.
     df = df.replace(to_replace="Labour",
                     value={
                         "party": "Labour"
                     })
+    # 2(a) ii.
     most_common_parties = set(df["party"]
                                .value_counts()
                                .sort_values(ascending=False)
                                .index[:n_parties])
-    print(most_common_parties)
+    df = df[df["party"].isin(most_common_parties)]
 
+    return df
 
 if __name__ == "__main__":
     df = pd.read_csv(DATASET_PATH)
-    clean_hansard(df)
+    print(df["party"].tail())
+    print(clean_hansard(df)["party"].tail())
