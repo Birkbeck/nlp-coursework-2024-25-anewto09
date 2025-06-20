a15b82af72c3841f5e274a3a90c3ba8959a5729d
anewto09 <anewto09@student.bbk.ac.uk>
2025-06-20 20:22:23 +0100

    Getting most common parties

--- PartTwo.py
+++ PartTwo.py
@@ -2,7 +2,19 @@ import pathlib
 import pandas as pd
 
 DATASET_PATH = pathlib.Path(__file__).parent / "p2-texts" / "hansard40000.csv"
+
+def clean_hansard(df: pd.DataFrame, n_parties: int = 4) -> pd.DataFrame:
+    df = df.replace(to_replace="Labour",
+                    value={
+                        "party": "Labour"
+                    })
+    most_common_parties = set(df["party"]
+                               .value_counts()
+                               .sort_values(ascending=False)
+                               .index[:n_parties])
+    print(most_common_parties)
+
+
 if __name__ == "__main__":
     df = pd.read_csv(DATASET_PATH)
-    print(df.head())
-    print(df.info())
+    clean_hansard(df)
