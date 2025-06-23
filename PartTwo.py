269748b2adff579769343a0a4b345b391451a6f1
anewto09 <anewto09@student.bbk.ac.uk>
2025-06-23 14:10:10 +0100

    first custom tokeniser attempt

--- PartTwo.py
+++ PartTwo.py
@@ -1,5 +1,7 @@
 import pathlib
 import pandas as pd
+import re
+from nltk.corpus import wordnet
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -41,6 +43,36 @@ def try_vectoriser(vectoriser, print_f1_macroavg: bool = False):
         print(f"{name} classification report:")
         print(classification_report(party_test, party_pred))
 
+def normalise_synonyms(token: str, syn_map: dict[str, str]) -> str:
+    """Converts a token such that when this function is applied to all (initial) tokens, synonyms end up as the same token"""
+    # first, ignore case and morphology
+    token = token.lower()
+    token = wordnet.morphy(token) or token 
+
+    # if already have established 'normalised' form then can return this
+    if token in syn_map:
+        return syn_map[token]
+
+    # otherwise we will make this the 'normalised' form for this word and its synonyms
+    synonyms = wordnet.synonyms(token)
+    
+    if not synonyms:
+        return token
+    
+    synonyms = {syn for syn_list in synonyms for syn in syn_list}
+    synonyms.add(token)
+    syn_map.update({syn: token for syn in synonyms})
+
+    return token
+
+def custom_tokeniser(text: str) -> list[str]:
+    # split on non-alphanumeric characters, except apostrophes
+    tokens = re.split(r"[^\w']+", text)
+    # lump synonyms into same token
+    syn_map = {}
+    tokens = [normalise_synonyms(t, syn_map) for t in tokens]
+    return tokens
+
 if __name__ == "__main__":
     # part (a)
     df = pd.read_csv(DATASET_PATH)
@@ -56,8 +88,14 @@ if __name__ == "__main__":
         stratify=df["party"]
     )
     
-    print("Unigrams only:")
-    try_vectoriser(TfidfVectorizer(stop_words='english', max_features=3000), True)
+    # part (b) / (c)
+    # print("Unigrams only:")
+    # try_vectoriser(TfidfVectorizer(stop_words='english', max_features=3000), True)
 
-    print("Unigrams, bigrams, and trigrams:")
-    try_vectoriser(TfidfVectorizer(stop_words='english', max_features=3000, ngram_range=(1, 3)))
+    # # part (d)
+    # print("Unigrams, bigrams, and trigrams:")
+    # try_vectoriser(TfidfVectorizer(stop_words='english', max_features=3000, ngram_range=(1, 3)))
+
+    # part (e)
+    print("Custom tokeniser:")
+    try_vectoriser(TfidfVectorizer(stop_words='english', max_features=3000, tokenizer=custom_tokeniser))
